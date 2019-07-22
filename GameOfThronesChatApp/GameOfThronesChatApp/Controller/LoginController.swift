5a7250a3882da4f398973439e0139c8d76460e45
Eric Blancas <ericblancas23@gmail.com>
2019-07-22 15:39:44 -0700

    adding more constraints

--- GameOfThronesChatApp/GameOfThronesChatApp/Controller/LoginController.swift
+++ GameOfThronesChatApp/GameOfThronesChatApp/Controller/LoginController.swift
@@ -22,8 +22,18 @@ class LoginController: UIViewController {
     let loginRegisterButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = UIColor(r: 80, g: 101, b: 161)
+        button.backgroundColor = UIColor(r: 80, g: 101, b: 200)
+        button.layer.cornerRadius = 5
         button.setTitle("Register", for: .normal)
+        return button
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor(r: 80, g: 101, b: 200)
+        button.layer.cornerRadius = 5
+        button.setTitle("Login", for: .normal)
         return button
     }()
 
@@ -44,12 +54,13 @@ class LoginController: UIViewController {
 //        inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
         setupInputContainerView()
         setupLoginRegisterButton()
+        setupLoginButton()
     }
     
     func setupInputContainerView() {
         //need x, y width, height constraints
         inputContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -110).isActive = true
         inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
@@ -57,9 +68,13 @@ class LoginController: UIViewController {
     func setupLoginRegisterButton() {
         loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         loginRegisterButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        loginRegisterButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -15).isActive = true
-        loginRegisterButton.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        loginRegisterButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -200).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalTo: loginRegisterButton.heightAnchor, constant: -50).isActive = true
 
+    }
+    
+    func setupLoginButton() {
+        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {

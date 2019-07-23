ae9e8c68b82504c2185441c1ef2995e5b7b52a46
Eric Blancas <ericblancas23@gmail.com>
2019-07-23 10:19:14 -0700

    setting up text field

--- GameOfThronesChatApp/GameOfThronesChatApp/Controller/LoginController.swift
+++ GameOfThronesChatApp/GameOfThronesChatApp/Controller/LoginController.swift
@@ -26,10 +26,23 @@ class LoginController: UIViewController {
         button.layer.cornerRadius = 5
         button.setTitle("Register", for: .normal)
         button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         return button
     }()
     
-
+    let nameTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Name"
+        return tf
+    }()
+    
+    let nameSeperator: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(r: 220, g: 220, b: 220)
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -51,14 +64,29 @@ class LoginController: UIViewController {
         inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -110).isActive = true
         inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        inputContainerView.addSubview(nameTextField)
+        inputContainerView.addSubview(nameSeperator)
+        
+        //constraints x, y
+        nameTextField.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor, constant: 12).isActive = true
+        nameTextField.topAnchor.constraint(equalTo: inputContainerView.topAnchor).isActive = true
+        nameTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        nameTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        //constraints x, y
+        nameSeperator.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor).isActive = true
+        nameSeperator.topAnchor.constraint(equalTo: nameTextField.bottomAnchor).isActive = true
+        nameSeperator.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        nameSeperator.heightAnchor.constraint(equalToConstant: 1).isActive = true
     }
     
     func setupLoginRegisterButton() {
         loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         loginRegisterButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         loginRegisterButton.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
-        loginRegisterButton.heightAnchor.constraint(equalTo: loginRegisterButton.heightAnchor, constant: -50).isActive = true
-
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
     }
     
     
